5b266322d921e5d83ac6e8715717a0259f6c1257
jarett-lee <jarett.lee.pi@gmail.com>
2017-09-30 11:08:59 -0400

    Add simulation body

--- src/org/dyn4j/ex/Car.java
+++ src/org/dyn4j/ex/Car.java
@@ -1,10 +1,13 @@
 package org.dyn4j.ex;
 
 import org.dyn4j.dynamics.World;
+import org.dyn4j.samples.SimulationBody;
 import org.dyn4j.samples.SimulationFrame;
 
 public class Car extends SimulationFrame{
 	private static final long serialVersionUID = -6715546875088615532L;
+	
+	private SimulationBody car;
 
 	public Car() {
 		super("Car", 32.0);
@@ -18,7 +21,9 @@ public class Car extends SimulationFrame{
 	@Override
 	protected void initializeWorld() {
 		this.world.setGravity(World.ZERO_GRAVITY);
-		// TODO Auto-generated method stub
 
+		// the car
+		car = new SimulationBody();
+		this.world.addBody(car);
 	}
 }
