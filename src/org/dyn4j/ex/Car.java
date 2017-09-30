35917aca05dc6e555c562860542d29a4081fb1c1
jarett-lee <jarett.lee.pi@gmail.com>
2017-09-30 11:56:35 -0400

    Add wheels

--- src/org/dyn4j/ex/Car.java
+++ src/org/dyn4j/ex/Car.java
@@ -7,10 +7,27 @@ import org.dyn4j.samples.SimulationBody;
 
 public class Car extends SimulationBody {
 	
+	private BodyFixture frontWheel1;
+	private BodyFixture frontWheel2;
+	
 	public Car() {
 		this.addFixture(Geometry.createRectangle(0.5, 1.5), 1, 0.2, 0.2);
+
 		BodyFixture bf2 = this.addFixture(Geometry.createEquilateralTriangle(0.5), 1, 0.2, 0.2);
 		bf2.getShape().translate(0, 0.9);
+		
+		frontWheel1 = this.addFixture(Geometry.createRectangle(0.2, 0.5), 1, 0.7, 0.2);
+		frontWheel1.getShape().translate(-0.2, 0.5);
+
+		frontWheel2 = this.addFixture(Geometry.createRectangle(0.2, 0.5), 1, 0.7, 0.2);
+		frontWheel2.getShape().translate(0.2, 0.5);
+
+		BodyFixture wheel = this.addFixture(Geometry.createRectangle(0.2, 0.5), 1, 0.2, 0.2);
+		wheel.getShape().translate(-0.2, -0.5);
+
+		wheel = this.addFixture(Geometry.createRectangle(0.2, 0.5), 1, 0.2, 0.2);
+		wheel.getShape().translate(0.2, -0.5);
+
 		this.translate(0.0, 2.0);
 		this.setMass(MassType.NORMAL);
 
--- src/org/dyn4j/ex/CarSimulation.java
+++ src/org/dyn4j/ex/CarSimulation.java
@@ -1,11 +1,16 @@
 package org.dyn4j.ex;
 
+import java.awt.Graphics2D;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.Line2D;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.dyn4j.dynamics.World;
+import org.dyn4j.geometry.Geometry;
+import org.dyn4j.geometry.MassType;
+import org.dyn4j.geometry.Vector2;
 import org.dyn4j.samples.SimulationBody;
 import org.dyn4j.samples.SimulationFrame;
 
@@ -67,9 +72,74 @@ public class CarSimulation extends SimulationFrame{
 	@Override
 	protected void initializeWorld() {
 		this.world.setGravity(World.ZERO_GRAVITY);
+		
+		double offset = 8.0;
+
+		SimulationBody l = new SimulationBody();
+		l.addFixture(Geometry.createRectangle(1, 15));
+		l.translate(-offset, 0);
+		l.setMass(MassType.INFINITE);
+		this.world.addBody(l);
+
+		SimulationBody r = new SimulationBody();
+		r.addFixture(Geometry.createRectangle(1, 15));
+		r.translate(offset, 0);
+		r.setMass(MassType.INFINITE);
+		this.world.addBody(r);
+		
+		SimulationBody t = new SimulationBody();
+		t.addFixture(Geometry.createRectangle(15, 1));
+		t.translate(0, offset);
+		t.setMass(MassType.INFINITE);
+		this.world.addBody(t);
+		
+		SimulationBody b = new SimulationBody();
+		b.addFixture(Geometry.createRectangle(15, 1));
+		b.translate(0, -offset);
+		b.setMass(MassType.INFINITE);
+		this.world.addBody(b);
 
 		// the car
 		car = new Car();
 		this.world.addBody(car);
+	}
+
+	@Override
+	protected void update(Graphics2D g, double elapsedTime) {
+		super.update(g, elapsedTime);
+		
+		final double scale = this.scale;
+		final double force = 500 * elapsedTime;
+
+        final Vector2 r = new Vector2(car.getTransform().getRotation() + Math.PI * 0.5);
+        final Vector2 c = car.getWorldCenter();
+
+        double angle = 0;
+        if (this.leftTurnOn.get()) {
+        	angle = 15;
+        }
+
+		// apply thrust
+        if (this.forwardThrustOn.get()) {
+        	Vector2 f1 = r.product(force);
+        	Vector2 p1 = c.sum(r.product(0.9).rotate(30/360.0 * Math.PI));
+        	Vector2 f2 = r.product(force);
+        	Vector2 p2 = c.sum(r.product(0.9).rotate(-30/360.0 * Math.PI));
+        	f1.rotate(angle/360.0 * Math.PI);
+        	f2.rotate(angle/360.0 * Math.PI);
+        	
+        	car.applyForce(f1, p1);
+        	car.applyForce(f2, p2);
+        	
+//        	g.setColor(Color.ORANGE);
+        	g.draw(new Line2D.Double(p1.x * scale, p1.y * scale, (p1.x - f1.x) * scale, (p1.y - f1.y) * scale));
+        	g.draw(new Line2D.Double(p2.x * scale, p2.y * scale, (p2.x - f2.x) * scale, (p2.y - f2.y) * scale));
+        } 
+
+        if (this.rightTurnOn.get()) {
+        	Vector2 f = r.product(force).right();
+        	Vector2 p = c.sum(r.product(0.9));
+        	car.applyForce(f, p);
+        } 
 	}
 }
