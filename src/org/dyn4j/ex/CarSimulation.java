b6cf43c875b9460c181c563d15bb18030a424cb7
jarett-lee <jarett.lee.pi@gmail.com>
2017-09-30 11:15:32 -0400

    Add key listener

--- src/org/dyn4j/ex/CarSimulation.java
+++ src/org/dyn4j/ex/CarSimulation.java
@@ -1,5 +1,10 @@
 package org.dyn4j.ex;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.dyn4j.dynamics.World;
 import org.dyn4j.samples.SimulationBody;
 import org.dyn4j.samples.SimulationFrame;
@@ -9,8 +14,49 @@ public class CarSimulation extends SimulationFrame{
 	
 	private SimulationBody car;
 
+	private AtomicBoolean forwardThrustOn = new AtomicBoolean(false);
+	private AtomicBoolean leftTurnOn = new AtomicBoolean(false);
+	private AtomicBoolean rightTurnOn = new AtomicBoolean(false);
+
+	private class CustomKeyListener extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			switch (e.getKeyCode()) {
+				case KeyEvent.VK_UP:
+					forwardThrustOn.set(true);
+					break;
+				case KeyEvent.VK_LEFT:
+					leftTurnOn.set(true);
+					break;
+				case KeyEvent.VK_RIGHT:
+					rightTurnOn.set(true);
+					break;
+			}
+			
+		}
+		
+		@Override
+		public void keyReleased(KeyEvent e) {
+			switch (e.getKeyCode()) {
+				case KeyEvent.VK_UP:
+					forwardThrustOn.set(false);
+					break;
+				case KeyEvent.VK_LEFT:
+					leftTurnOn.set(false);
+					break;
+				case KeyEvent.VK_RIGHT:
+					rightTurnOn.set(false);
+					break;
+			}
+		}
+	}
+
 	public CarSimulation() {
 		super("Car", 32.0);
+
+		KeyListener listener = new CustomKeyListener();
+		this.addKeyListener(listener);
+		this.canvas.addKeyListener(listener);
 	}
 
 	public static void main(String[] args) {
